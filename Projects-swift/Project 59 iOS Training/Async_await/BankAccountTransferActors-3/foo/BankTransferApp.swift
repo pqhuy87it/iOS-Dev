4c332b6c7378c52b523e4fdbedc55754fe84d296
Phạm Huy(みかん坊や) <pqhuy87it@gmail.com>
2026-05-20 23:10:51 +0700

    update

---
+++ Projects-swift/Project 59 iOS Training/Async_await/BankAccountTransferActors-3/foo/BankTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  fooApp.swift
+//  foo
+//
+//  Created by Mohammad Azam on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BankTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects-swift/Project 59 iOS Training/Async_await/BankAccountTransferActors-3/foo/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  foo
+//
+//  Created by Mohammad Azam on 7/23/21.
+//
+
+import SwiftUI
+
+enum BankError: Error {
+    case insufficientFunds(Double)
+}
+
+actor BankAccount {
+    
+    let accountNumber: Int
+    var balance: Double
+    
+    init(accountNumber: Int, balance: Double) {
+        self.accountNumber = accountNumber
+        self.balance = balance
+    }
+    
+    nonisolated func getCurrentAPR() -> Double {
+        return 0.2
+    }
+    
+    func deposit(_ amount: Double) {
+        balance += amount
+    }
+    
+    func transfer(amount: Double, to other: BankAccount) async throws {
+        if amount > balance {
+            throw BankError.insufficientFunds(amount)
+        }
+        
+        balance -= amount
+        await other.deposit(amount)
+        
+        print(other.accountNumber)
+        print("Current Account: \(balance), Other Account: \(await other.balance)")
+    }
+}
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        Button {
+            
+            let bankAccount = BankAccount(accountNumber: 123, balance: 500)
+            let otherAccount = BankAccount(accountNumber: 456, balance: 100)
+            
+            let _ = bankAccount.getCurrentAPR()
+              
+            DispatchQueue.concurrentPerform(iterations: 100) { _ in
+                Task {
+                    try? await bankAccount.transfer(amount: 300, to: otherAccount)
+                }
+            }
+            
+        } label: {
+            Text("Transfer")
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
